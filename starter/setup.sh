c175d51892d8566c5022f9195b1b7455623c400f
Dijon Mboko <dijonmboko@gmail.com>
2020-10-20 11:59:10 +0200

    remove exports

--- starter/setup.sh
+++ starter/setup.sh
@@ -1,4 +1,1 @@
-#export AUTH0_DOMAIN='udacity-capstone-project.auth0.com'
-#export ALGORITHMS=['RS256']
-#export API_AUDIENCE='capstone-app'
-#export SQLALCHEMY_DATABASE_URI='postgres://localhost:5432/capstone_db'
+
